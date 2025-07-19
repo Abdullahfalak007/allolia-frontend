892c02cb798efa7afee28ef964f23af132109cf7
Abdullahfalak007 <abdullahfalak007@gmail.com>
2025-07-20 01:45:22 +0500

    Hero Section Fixed!

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -9,9 +9,9 @@ const Hero: React.FC = () => {
       style={{ backgroundImage: `url(${images.heroBg})` }}
     >
       <div className="absolute inset-0 bg-black/40"></div>
-      <div className="relative z-10 mx-auto px-2 sm:px-4 md:px-32 py-20 md:py-32 flex flex-col md:flex-row items-center ">
+      <div className="relative z-10 mx-auto px-2 sm:px-4 md:px-32 lg:px-20 py-20 md:py-32 flex flex-col lg:flex-row items-center ">
         {/* Left column */}
-        <div className="md:w-1/2 text-center md:text-left space-y-12 mt-12">
+        <div className="lg:w-1/2 text-center md:text-left space-y-12 mt-12">
           {/* Title */}
           <h1 className="text-white text-3xl sm:text-4xl lg:text-4xl font-bold ">
             Service client spécialisé en location courte durée
@@ -65,7 +65,7 @@ const Hero: React.FC = () => {
         </div>
 
         {/* Right card with hero shape image, frosted glass effect, and overlayed content */}
-        <div className="md:w-1/2 mt-12 md:mt-0 flex justify-center md:justify-end">
+        <div className="hidden lg:block md:w-1/2 mt-12 md:mt-0 flex justify-center md:justify-end">
           <div className="relative mt-48 max-w-sm w-full">
             {/* Hero shape image as background, semi-transparent */}
             <img
